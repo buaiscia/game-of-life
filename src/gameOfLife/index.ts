bd95d60f924d0f38300ded4650ac3580f6fb3809
Alex Buaiscia <alex.buaiscia@gmail.com>
2021-03-06 16:20:17 +0100

    refactor: divided checkNeighbors

--- src/gameOfLife/index.ts
+++ src/gameOfLife/index.ts
@@ -18,7 +18,7 @@ export default class GameOfLife {
 
         for (let row = 0; row < this.grid.length; row++) {
             for (let col = 0; col < this.grid.length; col++) {
-                aliveOrDead = this.checkNeighbors(this.grid, row, col);
+                aliveOrDead = this.cellState(this.grid, row, col);
                 this.grid[row][col] = aliveOrDead;
             }
         }
@@ -26,31 +26,36 @@ export default class GameOfLife {
         console.log(this.grid);
     }
 
-    private checkNeighbors(grid: number[][], row: number, col: number): number {
-        const { top, topRight, right, bottomRight, bottom, bottomLeft, left, topLeft } = this.positions(grid, row, col);
-        let alive = 0;
-        alive += this.checkNeighborCell(top);
-        alive += this.checkNeighborCell(topRight);
-        alive += this.checkNeighborCell(right);
-        alive += this.checkNeighborCell(bottomRight);
-        alive += this.checkNeighborCell(bottom);
-        alive += this.checkNeighborCell(bottomLeft);
-        alive += this.checkNeighborCell(left);
-        alive += this.checkNeighborCell(topLeft);
-
+    private cellState(grid: number[][], row: number, col: number): number {
+        const aliveCount = this.checkNeighbors(grid, row, col);
         if (grid[row][col]) {
-            if (alive < 2 || alive > 3) {
+            if (aliveCount < 2 || aliveCount > 3) {
                 return 0;
             }
             return 1;
         }
         else {
-            if (alive === 3) {
+            if (aliveCount === 3) {
                 return 1;
             }
 
             return 0;
         }
+    }
+
+    private checkNeighbors(grid: number[][], row: number, col: number): number {
+        const { top, topRight, right, bottomRight, bottom, bottomLeft, left, topLeft } = this.positions(grid, row, col);
+        let aliveCount = 0;
+        aliveCount += this.checkNeighborCell(top);
+        aliveCount += this.checkNeighborCell(topRight);
+        aliveCount += this.checkNeighborCell(right);
+        aliveCount += this.checkNeighborCell(bottomRight);
+        aliveCount += this.checkNeighborCell(bottom);
+        aliveCount += this.checkNeighborCell(bottomLeft);
+        aliveCount += this.checkNeighborCell(left);
+        aliveCount += this.checkNeighborCell(topLeft);
+
+        return aliveCount;
     }
 
     private positions(grid: number[][], row: number, col: number): Positions {
