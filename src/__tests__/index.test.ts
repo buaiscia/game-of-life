df64333832a9cb960df4ca272e5a9cb3399c389a
Alex Buaiscia <alex.buaiscia@gmail.com>
2021-03-08 13:50:47 +0100

    test: uncomment tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -13,29 +13,29 @@ describe('e2e test', () => {
         expect(updatedGrid[0][0]).toBe(1);
     });
 
-    // it('should throw an error with wrong array format', () => {
-    //     const coordinates = [1, 2];
-    //     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //     // @ts-ignore
-    //     expect(() => init(coordinates, gridFormat)).toThrowError(/Wrong format of coordinates/);
-    // });
+    it('should throw an error with wrong array format', () => {
+        const coordinates = [1, 2];
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        expect(() => init(coordinates, gridFormat)).toThrowError(/Wrong format of coordinates/);
+    });
 
-    // it('should throw an error with no array input', () => {
-    //     const coordinates = 5;
-    //     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //     // @ts-ignore
-    //     expect(() => init(coordinates, gridFormat)).toThrowError(/Wrong format of coordinates/);
-    // });
+    it('should throw an error with no array input', () => {
+        const coordinates = 5;
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        expect(() => init(coordinates, gridFormat)).toThrowError(/Wrong format of coordinates/);
+    });
 
-    // it('should continue without array element out of boundary', () => {
-    //     const newCoordinates: Coordinates[] = [[0, 1], [0, 2], [1, 0], [1, 1], [1, 20], [20, 1]];
-    //     const updatedGrid = init(newCoordinates, gridFormat);
-    //     expect(updatedGrid[0][0]).toBe(1);
-    // });
+    it('should continue without array element out of boundary', () => {
+        const newCoordinates: Coordinates[] = [[0, 1], [0, 2], [1, 0], [1, 1], [1, 20], [20, 1]];
+        const updatedGrid = init(newCoordinates, gridFormat);
+        expect(updatedGrid[0][0]).toBe(1);
+    });
 
-    // it('should throw an error without grid format specification', () => {
-    //     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //     // @ts-ignore
-    //     expect(() => init(coordinates)).toThrowError(/Missing rows or columns input number/);
-    // });
+    it('should throw an error without grid format specification', () => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        expect(() => init(coordinates)).toThrowError(/Missing rows or columns input number/);
+    });
 });
